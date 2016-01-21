583db604b74eefdf78d7e2b13003c6b2949b6d64
gorranian <gorranian@yahoo.com.br>
2016-01-21 21:06:39 -0200

    Created basic menu

---
+++ ExpressNodeTest/ExpressNodeTest/public/javascripts/init.js
@@ -0,0 +1,34 @@
+﻿//Define which item is currently in focus on the menu
+var focusItem = 0;
+
+$(document).ready(function () {
+    setFocus(focusItem);
+    $(".menuitem").on("click", function () {
+        setFocus(1);
+    });
+});
+
+function setFocus(item){
+    $("#item" + item).animate({
+        top: "45%",
+        left: "42%",
+        opacity: "0.9",
+        zoom: "2"
+    }, 800);
+    if (item - 1 >= 0) {
+        $("#item" + (item - 1)).animate({
+            top: "75%",
+            left: "15%",
+            opacity: "0.5",
+            zoom: "1"
+        }, 800);
+    }
+    if ($("#item" + (item + 1)).length > 0) {
+        $("#item" + (item + 1)).animate({
+            top: "75%",
+            left: "75%",
+            opacity: "0.5",
+            zoom: "1"
+        }, 800);
+    }
+}
