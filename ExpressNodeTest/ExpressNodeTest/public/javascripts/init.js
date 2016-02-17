b2fbd93d806e59a1b8379a55441021cc67da6db1
gorranian <gorranian@yahoo.com.br>
2016-02-17 21:13:51 -0200

    More content

--- ExpressNodeTest/ExpressNodeTest/public/javascripts/init.js
+++ ExpressNodeTest/ExpressNodeTest/public/javascripts/init.js
@@ -5,15 +5,30 @@ $(document).ready(function () {
     setFocus(focusItem);
     $(".navbarright").on("click", function () {
         focusItem = Math.min(focusItem + 1, navitems.length - 1);
-        setFocus(focusItem);
+        setFocus(focusItem, "right");
     });
     $(".navbarleft").on("click", function () {
         focusItem = Math.max(focusItem - 1, 0);
-        setFocus(focusItem);
+        setFocus(focusItem, "left");
     });
 });
 
-function setFocus(item){
+function setFocus(item, direction){
+    if (typeof (direction) === undefined) direction = "left";
+    $(".contentarea").animate({
+        opacity: "0",
+        left:direction == "right" ? "-20%" : "120%"
+    }, 400, function () {
+        $(".contentarea").css('left', direction == "right" ? "120%" : "-20%");
+        $(".contentarea").animate({
+            opacity: "1",
+            left: "10%"
+        }, 400);
+        navitems.forEach(function (entry) {
+            $("#" + entry).css('display', 'none');
+        });
+        $("#" + navitems[item]).css('display', 'block');
+    });
     $("#item" + item).animate({
         top: "45%",
         left: "42%",
